ce538698f853cd08d4a55fc7b75828c5fd80c25a
Jim Martens <jim1@live.de>
2013-09-21 15:14:16 +0200

    Added build script and config file.

---
+++ src/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# variables declaration
+ultimateCMSDir="../../de.plugins-zum-selberbauen.ultimate/"
+buildDir="../../Ultimate-CMS-Minify-JS/"
+configFile="config.txt"
+lastCheckedCommit="$(cat $configFile)"
+yuiFile="yuicompressor-2.4.8.jar"
+
+# determining modified JS files
+cd "$ultimateCMSDir"
+git checkout dev
+if [ "$lastCheckedCommit" = "" ]; then
+	# minimize all JS files
+	ls src/js/*.js src/acp/js/*.js | \
+	grep "\.js$" \
+	xargs -d "\n" -n 1 -r \
+	java -jar "$buildDir$yuiFile" \
+	 --preserve-semi -o ".js$:.min.js" *.js
+else
+	# minimize changed JS files
+	git diff --name-status "$lastCheckedCommit" HEAD | \
+	grep -v "^D" | \
+	sed "s/^.\s*//" | \
+	grep "[^\.]+\.js$" | \
+	xargs -d "\n" -n 1 -r \
+	java -jar "$buildDir$yuiFile" \
+	 --preserve-semi -o ".js$:.min.js" *.js
+fi
+
+# commit changes
+git add --all
+git commit -m "Updating minified JavaScript files"
+git push origin dev
+
+# save current commit to config.txt
+git rev-parse HEAD > "$buildDir$configFile"
